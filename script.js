3cff29b0b658a19b71981fb526f6574518f0ddbb
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2024-01-07 12:30:58 +0530

    practiced js

--- script.js
+++ script.js
@@ -14,6 +14,26 @@ function type() {
   }
 }
 
+// let typetext = "User is Typing...";
+// let tpg = 0;
+
+// function typing() {
+//   if (tpg <= typetext.length) {
+//     document.getElementById("typing-txt").innerText = typetext.slice(0, tpg);
+//     tpg++;
+//     setTimeout(typing, 100);
+//   } else {
+//     setTimeout(function () {
+//       document.querySelector(".cursor").style.display = "inline";
+//     }, 3000);
+//   }
+// }
+
+// console.log("This is Slice:", typetext.slice(10, 15)); // Using slice with start and end index
+// console.log("This is Length:", typetext.length);
+// console.log("This is Split:", typetext.split(" ")); // Using split with a space as the separator
+// console.log("This is CharCodeAT:", typetext.charCodeAt(0)); // Using charCodeAt with the index
+
 // Start the typing effect when the page loads
 window.onload = function () {
   type();
